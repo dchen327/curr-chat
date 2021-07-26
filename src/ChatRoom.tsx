352e077b15b39534a5b3345e05af9e635176e646
David Chen <davidc3287@gmail.com>
2021-07-26 08:48:03 -0400

    add chat input box

--- src/ChatRoom.tsx
+++ src/ChatRoom.tsx
@@ -5,15 +5,22 @@ function ChatRoom() {
 
   return (
     <>
-      <form onSubmit={() => console.log("submitted")}>
-        <input
-          value={formValue}
-          onChange={(e) => setFormValue(e.target.value)}
-          placeholder="type to chat!"
-        />
-        <button type="submit" className="button">
-          Send
-        </button>
+      <form onSubmit={() => console.log(formValue)}>
+        <div className="field has-addons">
+          <div className="control is-expanded">
+            <input
+              className="input"
+              value={formValue}
+              onChange={(e) => setFormValue(e.target.value)}
+              placeholder="Type to chat!"
+            />
+          </div>
+          <div className="control">
+            <button type="submit" className="button is-primary">
+              Send
+            </button>
+          </div>
+        </div>
       </form>
     </>
   );
