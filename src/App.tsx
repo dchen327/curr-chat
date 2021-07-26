1926640e251102f43ddbf1fbbc2a6f54c195630d
David Chen <davidc3287@gmail.com>
2021-07-26 08:22:35 -0400

    remove unnecessary files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from "react";
-import logo from "./logo.svg";
 import "bulma/css/bulma.min.css";
 
 function App() {
   return (
-    <div className="container">
-      <button className="button is-primary">Hello World</button>
-    </div>
+    <>
+      <section className="hero is-info">
+        <div className="hero-body">
+          <p className="title">CurrChat</p>
+          <p className="subtitle">Real-time current events chat rooms</p>
+        </div>
+      </section>
+
+      <div className="container">
+        <button className="button is-primary">Hello World</button>
+      </div>
+    </>
   );
 }
 
