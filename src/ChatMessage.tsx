00aa6e26e6f3c735c251bd9be64d7242103df16f
David Chen <davidc3287@gmail.com>
2021-07-26 22:45:35 -0400

    add icons

--- src/ChatMessage.tsx
+++ src/ChatMessage.tsx
@@ -2,6 +2,12 @@ function ChatMessage({ name, message, isOwnMessage }: any) {
   return (
     <p className={`py-3 ${isOwnMessage ? "has-text-right" : ""}`}>
       <label className="px-2 label">{name}</label>
+      <span className="label icon-text">
+        <span className="icon">
+          <i className="fas fa-arrow-right"></i>
+        </span>
+        <span>Home</span>
+      </span>
       <span className={`tag is-large ${isOwnMessage ? "is-success" : ""}`}>
         {message}
       </span>
