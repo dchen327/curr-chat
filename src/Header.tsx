f207fd1db4ca2beb6db6a1028822a7ca8f3198fb
David Chen <davidc3287@gmail.com>
2021-07-26 20:53:51 -0400

    change name in header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,19 @@
 import { get, set } from "local-storage";
+import { useState } from "react";
 
 function Header() {
   const randID = 1000 + Math.floor(Math.random() * 9000); // random 4 digit ID
-  const username = get<string>("username") || `Guest ${randID}`;
+
+  // grab username from local storage or generate random username
+  const [username, setUsername] = useState(
+    get<string>("username") || `Guest ${randID}`
+  );
   set<string>("username", username);
+
+  const changeUsername = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    set<string>("username", username);
+  };
 
   return (
     <>
@@ -15,8 +25,28 @@ function Header() {
               <p className="subtitle">Real-time current events chat rooms</p>
             </div>
             <div className="column">
-              <label className="text">Your Name:</label>
-              <input className="input" placeholder={username} />
+              <form onSubmit={changeUsername}>
+                <label className="content">Your Name:</label>
+                <div className="field has-addons">
+                  <div className="control is-expanded">
+                    <input
+                      className="input"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                      placeholder={username}
+                    />
+                  </div>
+                  <div className="control">
+                    <button
+                      type="submit"
+                      disabled={!username}
+                      className="button is-primary"
+                    >
+                      Change
+                    </button>
+                  </div>
+                </div>
+              </form>
             </div>
           </div>
         </div>
