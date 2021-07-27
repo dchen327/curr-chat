586956309b99b2664f81e9a1718911550eb33630
David Chen <davidc3287@gmail.com>
2021-07-26 20:42:25 -0400

    clean up firebase;

--- src/index.tsx
+++ src/index.tsx
@@ -14,10 +14,7 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// if (firebase.apps.length === 0) {
-//   // don't reinitialize Firebase
 firebase.initializeApp(firebaseConfig);
-// }
 
 ReactDOM.render(
   <React.StrictMode>
